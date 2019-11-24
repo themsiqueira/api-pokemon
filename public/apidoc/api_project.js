5911589bff5ebdb0016ca2b870461c6930f7fc97
Mateus Siqueira <mateussiqueiracarneiro@outlook.com>
2019-11-24 10:15:23 -0300

    ApiDoc

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "api-pokemon",
+  "version": "0.1.0",
+  "description": "api para gerênciamento de pokemons",
+  "title": "Api Pokemon LiveOn",
+  "url": "http://localhost:3333/api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-24T13:14:26.496Z",
+    "url": "http://apidocjs.com",
+    "version": "0.18.0"
+  }
+});
